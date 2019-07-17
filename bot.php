dc7f0c300f985127df212b39b3d9e18c864161dc
Kaimook789 <48902387+Kaimook789@users.noreply.github.com>
2019-07-17 16:48:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -61,35 +61,32 @@ if ($msg_type == 'location') {
     );
 }
 elseif ($msg_type == 'sticker') {
-  $stickerurl = "https://stickershop.line-scdn.net/stickershop/v1/sticker/" . $stickerId . "/android/sticker.png";
+      $imageurl = "https://images.unsplash.com/photo-1486016006115-74a41448aea2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80";
       $mreply = array(
         'replyToken' => $replyToken,
         'messages' => array(
-          
-array(
+         array(
         'type' => 'flex',
         'altText' => 'Sticker!!',
         'contents' => array(
         'type' => 'bubble',
         'body' => array(
-          'type' => 'box',
-          'layout' => 'vertical',
-          'spacing' => 'md',
-          'contents' => array(
-            array(
-              'type' => 'text',
-          'align' => 'center',
-          'color' => '#049b1b',
-          'text' => 'USER : ' . $reline2
-      ),
-            array(
-          'type' => 'image',
-          'size' => '5xl',
-          'align' => 'center',
-          'url' => $stickerurl
-      )
-        )
-        )
+                      'type' => 'box',
+                      'layout' => 'vertical',
+                      'contents' => array(
+                                            array(
+                                              'type' => 'text',
+                                              'align' => 'center',
+                                              'color' => '#049b1b',
+                                              'text' => 'USER : ' . $reline2
+                                            ),
+                                            array(
+                                              'type' => 'image',
+                                              'align' => 'center',
+                                              'url' => $imageurl
+                                            )
+                                        )
+                    )
         )
         )
     )
