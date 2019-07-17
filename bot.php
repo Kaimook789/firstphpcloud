c3f4b8298a37bfbfb6db6253aeae04d4d83786cb
Kaimook789 <48902387+Kaimook789@users.noreply.github.com>
2019-07-17 17:07:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -63,34 +63,50 @@ if ($msg_type == 'location') {
 elseif ($msg_type == 'sticker') {
       $imageurl = "https://images.unsplash.com/photo-1486016006115-74a41448aea2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80";
       $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-         array(
-        'type' => 'flex',
-        'altText' => 'Sticker!!',
-        'contents' => array(
-        'type' => 'bubble',
-        'body' => array(
-                      'type' => 'box',
-                      'layout' => 'vertical',
-                      'contents' => array(
-                                            array(
-                                              'type' => 'text',
-                                              'align' => 'center',
-                                              'color' => '#049b1b',
-                                              'text' => 'USER : ' . $reline2
-                                            ),
-                                            array(
-                                              'type' => 'image',
-                                              'align' => 'center',
-                                              'url' => $imageurl
-                                            )
-                                        )
-                    )
-        )
-        )
-    )
-    );
+                'replyToken' => $replyToken,
+                'messages' => array(
+                                     array(
+                                    'type' => 'flex',
+                                    'altText' => 'Sticker!!',
+                                    'contents' => array(
+                                                    'type' => 'bubble',
+                                                    'body' => array(
+                                                                  'type' => 'box',
+                                                                  'layout' => 'vertical',
+                                                                  'contents' => array(
+                                                                                        array(
+                                                                                          'type' => 'text',
+                                                                                          'align' => 'center',
+                                                                                          'color' => '#049b1b',
+                                                                                          'text' => 'USER : ' . $reline2
+                                                                                        ),
+                                                                                        array(
+                                                                                          'type' => 'image',
+                                                                                          'align' => 'center',
+                                                                                          'url' => $imageurl
+                                                                                        )
+                                                                                    )
+                                                                )
+                                                    )
+                                                   ,'footer' => array(
+                                                                    'type' => 'box',
+                                                                    'layout' => 'vertical',
+                                                                    'contents' => array(
+                                                                                        array(
+                                                                                             'type' => 'button',
+                                                                                             'action'=> array(
+                                                                                             'type'=> 'uri',
+                                                                                             'label'=> 'Go.',
+                                                                                             'uri'=>: 'https://google.com'
+                                                                                           ),
+                                                                                        )
+                                                                                    )
+                                                                )
+                                                                    
+                                                   )
+                                    )
+                                )
+            );
 }
 else {
     $url = "https://bots.dialogflow.com/line/b215c49b-0b36-45b7-9db4-429ab4b0095a/webhook";
